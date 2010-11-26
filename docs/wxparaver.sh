8cf689e87e9defd6e6ae646c06b8542163b31271
pedro <>
2010-11-26 16:33:34 +0000

    *** empty log message ***

--- docs/wxparaver.sh
+++ docs/wxparaver.sh
@@ -14,11 +14,11 @@ elif ! [ -d ${PARAVER_HOME}/lib/wxparaver ] ; then
   echo "wxparaver libraries not found in \$PARAVER_HOME/lib/wxparaver/"
   exit 1
 
-elif ! [ -e ${PARAVER_HOME}/bin/wxparaver ] ; then
+elif ! [ -e ${PARAVER_HOME}/bin/wxparaver.bin ] ; then
   echo "wxparaver binary not found in \$PARAVER_HOME/bin/"
   exit 1
   
 fi
 
-LD_LIBRARY_PATH=$PARAVER_HOME/lib/paraver-kernel:$PARAVER_HOME/lib/wxparaver:$LD_LIBRARY_PATH $PARAVER_HOME/bin/wxparaver "$@"
+LD_LIBRARY_PATH=$PARAVER_HOME/lib/paraver-kernel:$PARAVER_HOME/lib/wxparaver:$LD_LIBRARY_PATH $PARAVER_HOME/bin/wxparaver.bin "$@"
 	
