59f3dd839b8648947b39122073cda0ae1352721f
eloy <>
2013-07-24 14:14:32 +0000

    *** empty log message ***

--- helpcontents.cpp
+++ helpcontents.cpp
@@ -75,12 +75,17 @@ BEGIN_EVENT_TABLE( HelpContents, wxDialog )
 
 ////@begin HelpContents event table entries
   EVT_HTML_LINK_CLICKED( ID_HTMLWINDOW, HelpContents::OnHtmlwindowLinkClicked )
+
   EVT_BUTTON( ID_BUTTON_INDEX, HelpContents::OnButtonIndexClick )
+
   EVT_BUTTON( ID_BITMAPBUTTON_BACK, HelpContents::OnBitmapbuttonBackClick )
   EVT_UPDATE_UI( ID_BITMAPBUTTON_BACK, HelpContents::OnBitmapbuttonBackUpdate )
+
   EVT_BUTTON( ID_BITMAPBUTTON_FORWARD, HelpContents::OnBitmapbuttonForwardClick )
   EVT_UPDATE_UI( ID_BITMAPBUTTON_FORWARD, HelpContents::OnBitmapbuttonForwardUpdate )
+
   EVT_BUTTON( ID_BUTTON_CLOSE, HelpContents::OnButtonCloseClick )
+
 ////@end HelpContents event table entries
 
 END_EVENT_TABLE()
@@ -384,7 +389,7 @@ void HelpContents::CreateControls()
   wxBitmap bmp( logoBSC_xpm );
 #endif
 
-  wxMemoryFSHandler::AddFile( "logoBSC.xpm",
+  wxMemoryFSHandler::AddFile( wxT( "logoBSC.xpm" ),
 #ifdef WIN32
                               bmp,
 #else
