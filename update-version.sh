2d6aa28c052718e6bc0ec639542ebd60b683c131
pedro <>
2015-03-04 17:09:24 +0000

    *** empty log message ***

---
+++ update-version.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+if [ $# -ne 1 ]; then
+  echo; echo "  Usage: $0 <new-version-tag>"
+  echo; echo "    Substitutes version tag in configure.ac and paravermain.h"
+        echo "    Before changes, they're copied to configure.ac.oldvers and paravermain.h.oldvers"
+        echo "    No quotes allowed"
+  echo; echo "  Example: $0 6.6.6"
+  echo
+  exit 1
+fi
+
+VERSION=${1}
+
+cp configure.ac configure.ac.oldvers
+export TAG_CONF=AC_INIT
+awk -F ',' \
+    '$0  ~ /^'${TAG_CONF}'/ { print $1 ", '${VERSION}' ," $3; } ; \
+     $0 !~ /^'${TAG_CONF}'/ { print $0; }' configure.ac > configure.ac.newchgvers
+mv configure.ac.newchgvers configure.ac
+
+
+cp paravermain.h paravermain.h.oldvers
+export TAG_CONF="#define\ VERSION"
+awk -F '"' \
+    '$0  ~ /^\"'${TAG_CONF}'\"/ { print '${TAG_CONF}' " \"" '${VERSION}' "\"" ; } ;  \
+     $0 !~ /^'${TAG_CONF}'/ { print $0; }'  paravermain.h > paravermain.h.newchgvers
+mv paravermain.h.newchgvers paravermain.h
+
